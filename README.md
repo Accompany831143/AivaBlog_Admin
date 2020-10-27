ac17976b4ec4f7cb3da28b5c599c43e0d3ca9076
Accompany831143 <accompany_zhao@163.com>
2020-10-27 10:42:29 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,17 @@
+# Aiva博客后台管理系统
+
+> 技术栈：React
+
+## 运行
+
+```bash
+# 安装依赖
+$ yarn install
+
+# 开发模式运行
+$ yarn start
+
+# 打包部署
+$ yarn build
+
+```
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Aiva-admin</h1>
+    </div>
+  );
+}
+
+export default App;
