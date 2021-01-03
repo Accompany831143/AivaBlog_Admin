0f17dc918f13be7551cae974bab4012fbde6bfa4
Accompany831143 <Accompany_zhao@163.com>
2021-01-03 20:01:06 +0800

    新建测试环境变量

--- src/Axios/envConst.js
+++ src/Axios/envConst.js
@@ -1,9 +1,18 @@
+/*
+ * @Date: 2020-11-14 20:29:33
+ * @LastEditors: Aiva
+ * @LastEditTime: 2021-01-03 19:55:35
+ * @FilePath: \AivaBlog_Admin\src\Axios\envConst.js
+ */
 const envObject = {
     development:{
         baseUrl:'http://localhost:7758/'
     },
+    test:{
+        baseUrl:'https://192.168.0.189:7758/'
+    },
     production:{
-        baseUrl:'https://aiva.vip/'
+        baseUrl:'https://aiva.vip:7758/'
     }
 
 }
